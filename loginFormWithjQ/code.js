5a364b379921941f249d20c266a7671d5b9163b5
Nazmia H. Alqawasmi <41819912+NazmiaHq@users.noreply.github.com>
2018-09-30 10:14:38 +0300

    Add files via upload

---
+++ loginFormWithjQ/code.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$(".them").hide();
+
+	$("#main").click(function(){
+    $(".them").show();
+    }); 
+	$("#btn2").click(function(){
+    $(".them").hide();
+    });
+    $("#shad").click(function(){
+    $(".them").hide();
+    });  
+
+
+}); 
